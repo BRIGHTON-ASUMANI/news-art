a06246bc0ba06d57131395a861b04f0a43b44f5d
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-25 16:22:33 +0300

    pushing my django appliaction on github after hours of coding

---
+++ news/tests.py
@@ -0,0 +1,53 @@
+from django.test import TestCase
+from .models import Editor,Article,tags
+
+# Create your tests here.
+
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.brighton= Editor(first_name = 'brighton', last_name ='Muriuki', email ='brighton@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.brighton,Editor))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.brighton.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+    # def tearDown(self):
+    #     Editor.objects.all().delete()
+
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.brighton= Editor(first_name = 'brighton', last_name ='Asumani', email ='brighton@gmail.com')
+        self.brighton.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.brighton)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
